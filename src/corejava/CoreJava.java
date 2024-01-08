1f11a887bbadd1e3fb04b542d103f56fbdc0eb77
mhasan17 <codewithmh17@gmail.com>
2024-01-08 17:29:18 +0600

    core java before OOPs upload done

--- src/corejava/CoreJava.java
+++ src/corejava/CoreJava.java
@@ -1,5 +1,6 @@
 package corejava;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -25,7 +26,14 @@ public class CoreJava {
         oddOrEven();
         
         Calling method calculator 
-        calculator();*/
+        calculator();
+        
+        Calling method smallest Among Three Numbers
+        smallestAmongThreeNumbers();
+ 
+        Calling method array Sorting that take input of 1D array and sort the array ascending 
+        array1D_Sorting();*/
+        array2D_Sorting();
     }
 
     public static void addTwoNumber() {
@@ -140,5 +148,99 @@ public class CoreJava {
                 System.out.println("Invalid choice.");
         }
 
+    }
+
+    public static void smallestAmongThreeNumbers() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any three integer numbers:");
+        int numberOne = input.nextInt();
+        int numberTwo = input.nextInt();
+        int numberThree = input.nextInt();
+
+        int result = ((numberOne < numberTwo) ? ((numberOne < numberThree) ? numberOne : numberThree) : (numberTwo < numberThree) ? numberTwo : numberThree);
+
+        System.out.println("Smallest number: " + result);
+
+    }
+
+    public static void array1D_Sorting() {
+        System.out.println("Please enter length of your array: ");
+        Scanner input = new Scanner(System.in);
+        int length = input.nextInt();
+
+        int[] array = new int[length];
+
+        for (int index = 0; index < length; index++) {
+            System.out.println("Enter an integer number to your array:");
+            array[index] = input.nextInt();
+        }
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+
+        for (int firstNumber = 0; firstNumber < array.length - 1; firstNumber++) {
+            for (int secondNumber = firstNumber + 1; secondNumber < array.length; secondNumber++) {
+                if (array[firstNumber] > array[secondNumber]) {
+                    int temporaryContainer = array[firstNumber];
+                    array[firstNumber] = array[secondNumber];
+                    array[secondNumber] = temporaryContainer;
+                }
+            }
+        }
+        System.out.print("Sorted Array: ");
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+
+    }
+
+    public static void array2D_Sorting() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter total number of row for your array: ");
+        int rowLength = input.nextInt();
+
+        System.out.println("Please enter total number of column for your array: ");
+        int columnLength = input.nextInt();
+
+        int[][] array2D = new int[rowLength][columnLength];
+
+        for (int row = 0; row < rowLength; row++) {
+            System.out.println("Enter integer numbers for the " + row + " position" + " row");
+            for (int column = 0; column < columnLength; column++) {
+                array2D[row][column] = input.nextInt();
+            }
+        }
+
+        System.out.println("Original Array: ");
+        for (int row = 0; row < array2D.length; row++) {
+            System.out.print("{ ");
+            for (int column = 0; column < array2D[row].length; column++) {
+                System.out.print(array2D[row][column] + " ");
+            }
+            System.out.print("}");
+            System.out.println("");
+        }
+
+        for (int firstRow = 0; firstRow < array2D.length; firstRow++) {
+            for (int firstColumnValue = 0; firstColumnValue < array2D[firstRow].length - 1; firstColumnValue++) {
+                for (int secondColumnValue = firstColumnValue + 1; secondColumnValue < array2D[firstRow].length; secondColumnValue++) {
+                    if (array2D[firstRow][firstColumnValue] > array2D[firstRow][secondColumnValue]) {
+                        int temporaryContainer = array2D[firstRow][firstColumnValue];
+                        array2D[firstRow][firstColumnValue] = array2D[firstRow][secondColumnValue];
+                        array2D[firstRow][secondColumnValue] = temporaryContainer;
+                    }
+                }
+            }
+        }
+        System.out.println("Sorted Array: ");
+
+        for (int row = 0; row < array2D.length; row++) {
+            System.out.print("{ ");
+            for (int column = 0; column < array2D[row].length; column++) {
+                System.out.print(array2D[row][column] + " ");
+            }
+            System.out.print("}");
+            System.out.println("");
+        }
     }
 }
